a194ae3c9eda799ae3cc07b7152afc7b50b123d9
binu <bnjasim@gmail.com>
2015-11-19 22:22:37 +0530

    Set up temporary canvas
    Add a canvasble directive to the sketch
    use link function inside this directive for mouse events
    set the width and height of the temp_canvas

--- paintangular.js
+++ paintangular.js
@@ -33,3 +33,20 @@ angular.module('main', [])
 	}
 })
 
+.directive('canvasable', function() {
+
+	return {
+		restrict: 'A',
+		require: '^paintangular',
+
+		link: function(scope, element, attrs, paintCtrl) {
+			var canvas = element.children().eq(0)[0];
+			var temp_canvas = element.children().eq(1)[0];
+			canvas.width = element[0].clientWidth;
+			canvas.height = element[0].clientHeight;
+			temp_canvas.width = canvas.width;
+			temp_canvas.height = canvas.height;
+			
+		}
+	}
+})
