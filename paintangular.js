1058905946126a323781504005a19226f28e4d8a
binu <bnjasim@gmail.com>
2015-11-19 21:49:07 +0530

    Log tools properly
    add ng-click to tools & pick size
    add one extra button to indicate the choosen color

--- paintangular.js
+++ paintangular.js
@@ -9,11 +9,23 @@ angular.module('main', [])
 
 		controller: ['$scope', function($scope) {
 			$scope.color = 'black'; // default stroke color
+			$scope.tools = 'pencil';
+			$scope.size = 2;
 
 			$scope.setColor = function(c) {
 				$scope.color = c;
 				console.log(c);
 			}
+
+			$scope.setTool = function(t) {
+				$scope.tool = t;
+				console.log(t);
+			}
+
+			$scope.chooseSize = function(s) {
+				$scope.size = s;
+				console.log(s);
+			}			
 		}],
 
 		templateUrl: "template.html"
